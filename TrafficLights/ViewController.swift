98e75d8376f0283059868181ecc510c602ca06f3
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2023-05-20 22:35:50 +0300

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Эльдар Абдуллин on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
